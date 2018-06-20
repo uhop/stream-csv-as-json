a150a2b97b9288eaf39b5c3b082a354db447ad51
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2018-06-20 09:57:43 -0500

    Added a test on tricky  values + a real CSV file.

--- tests/test_parser.js
+++ tests/test_parser.js
@@ -2,6 +2,12 @@
 
 const unit = require('heya-unit');
 
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+
+const {chain} = require('stream-chain');
+
 const Assembler = require('stream-json/Assembler');
 
 const Parser = require('../Parser');
@@ -24,6 +30,42 @@ unit.add(module, [
     });
     pipeline.on('end', () => {
       eval(t.TEST('t.unify(result, expected)'));
+      async.done();
+    });
+  },
+  function test_parser_tricky_values(t) {
+    const async = t.startAsync('test_parser_tricky_values');
+
+    const pipeline = chain([fs.createReadStream(path.resolve(__dirname, 'sample.csv.gz')), zlib.createGunzip(), new Parser()]);
+
+    let rows = 0,
+      empties = 0,
+      valuesWithCrLf = 0,
+      valuesWithDoubleQuote = 0;
+    pipeline.on('data', data => {
+      if (data.name === 'startArray') {
+        ++rows;
+        return;
+      }
+      if (data.name === 'stringValue') {
+        const value = data.value;
+        if (value) {
+          if (/[\u000A\u000D]/.test(value)) {
+            ++valuesWithCrLf;
+          }
+          if (/"/.test(value)) {
+            ++valuesWithDoubleQuote;
+          }
+        } else {
+          ++empties;
+        }
+      }
+    });
+    pipeline.on('end', () => {
+      eval(t.TEST('rows === 18127'));
+      eval(t.TEST('empties === 158989'));
+      eval(t.TEST('valuesWithCrLf === 1'));
+      eval(t.TEST('valuesWithDoubleQuote === 1'));
       async.done();
     });
   }
